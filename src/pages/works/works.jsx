8b1e830714f1866bbb58873eb2517711d32e06e9
fikayo <fikayoadele@gmail.com>
2025-09-20 12:33:02 +0100

    inal

--- src/pages/works/works.jsx
+++ src/pages/works/works.jsx
@@ -1,9 +1,5 @@
 import './works.css'
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { useEffect, useRef } from 'react';
-import { useInView } from "react-intersection-observer";
-import Slider from '../imgSlider/slider';
+
 
 export const Works = () => {
     const projects = [
@@ -11,37 +7,44 @@ export const Works = () => {
     id: 1,
     title: "Task Manager Microservices API",
     description: "A microservices-based API for managing tasks, users, and authentication with scalability in mind.",
-    image: "https://form.io/wp-content/uploads/thumbnail-formio-backend-api-integration.webp",
-    github: "https://github.com/yourname/fintech-wallet"
+    image: "./taskmanagerAPi.png",
+    github: "https://github.com/fikayo360/taskmanager"
   },
   {
     id: 2,
     title: "Movie Booking System API",
     description: "Backend API for booking movie tickets, managing schedules, and handling user reservations.",
-    image: "https://form.io/wp-content/uploads/thumbnail-formio-backend-api-integration.webp",
-    github: "https://github.com/yourname/job-scraper"
+    image: "movieBapi.png",
+    github: "https://github.com/fikayo360/movieBooking"
   },
   {
     id: 3,
     title: "Chess Backend API",
     description: "API that powers an online chess platform, handling game logic, moves, and multiplayer sessions.",
-    image: "https://form.io/wp-content/uploads/thumbnail-formio-backend-api-integration.webp",
-    github: "https://github.com/yourname/price-monitor"
+    image: "./chessApi.png",
+    github: "https://github.com/fikayo360/chessApi"
   },
   {
     id: 4,
     title: "E-commerce Review Sentiment Analysis Bot",
     description: "A bot that scrapes product reviews and performs sentiment analysis to classify customer feedback.",
-    image: "https://form.io/wp-content/uploads/thumbnail-formio-backend-api-integration.webp",
-    github: "https://github.com/yourname/workout-tracker"
+    image: "./sentimentReview.png",
+    github: "https://github.com/fikayo360/web-scrapping-projects/blob/main/trackingCustomerReviews.js"
   },
   {
     id: 5,
     title: "E-commerce Price Monitoring Bot",
     description: "Automated bot that tracks and monitors product prices across multiple e-commerce platforms.",
-    image: "https://form.io/wp-content/uploads/thumbnail-formio-backend-api-integration.webp",
-    github: "https://github.com/yourname/workout-tracker"
-  }
+    image: "./ecomPriMonitor.png",
+    github: "https://github.com/fikayo360/web-scrapping-projects/blob/main/priceMonitor.js"
+  },
+   {
+    id: 6,
+    title: "Instagram scrapper",
+    description: "Automated bot that scrapes user profiles from instagram",
+    image: "./instagramScrapper.png",
+    github: "https://github.com/fikayo360/web-scrapping-projects/blob/main/instagramProfile.js"
+  },
 ];
 
     return (
